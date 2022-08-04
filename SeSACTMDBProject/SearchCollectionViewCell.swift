29fb324718bc176731fd5b560287470e25205eba
ziuge <xwcjwwd@ewhain.net>
2022-08-04 22:22:26 +0900

    search cell UI

---
+++ SeSACTMDBProject/SearchCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchCollectionViewCell.swift
+//  SeSACTMDBProject
+//
+//  Created by CHOI on 2022/08/04.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
