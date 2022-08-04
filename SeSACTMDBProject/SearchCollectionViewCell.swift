aa20a62410ca233f00f619a58c4a095f79c56667
ziuge <xwcjwwd@ewhain.net>
2022-08-04 22:34:59 +0900

    collectionViewCell connect

--- SeSACTMDBProject/SearchCollectionViewCell.swift
+++ SeSACTMDBProject/SearchCollectionViewCell.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class SearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var actorLabel: UILabel!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
--- SeSACTMDBProject/SearchViewController.swift
+++ SeSACTMDBProject/SearchViewController.swift
@@ -9,12 +9,43 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 8
+        let width = UIScreen.main.bounds.width - (spacing * 2)
+        layout.itemSize = CGSize(width: width, height: width * 1.2)
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
+        
     }
-
     
+    
+}
 
+
+// MARK: CollectionView
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionViewCell", for: indexPath) as? SearchCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.backgroundColor = .lightGray
+        
+        return cell
+    }
+    
 }
