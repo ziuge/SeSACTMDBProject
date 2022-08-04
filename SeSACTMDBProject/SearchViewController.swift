e3a439e67218c71ceea2add91de5aa24e144a599
ziuge <xwcjwwd@ewhain.net>
2022-08-04 23:37:57 +0900

    패키지 설치, 네트워크 연결

--- SeSACTMDBProject/SearchViewController.swift
+++ SeSACTMDBProject/SearchViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+import Alamofire
+import Kingfisher
+import SwiftyJSON
+
 class SearchViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
@@ -19,7 +23,7 @@ class SearchViewController: UIViewController {
         
         
         let layout = UICollectionViewFlowLayout()
-        let spacing: CGFloat = 8
+        let spacing: CGFloat = 20
         let width = UIScreen.main.bounds.width - (spacing * 2)
         layout.itemSize = CGSize(width: width, height: width * 1.2)
         layout.scrollDirection = .vertical
@@ -28,22 +32,72 @@ class SearchViewController: UIViewController {
         layout.minimumInteritemSpacing = spacing
         collectionView.collectionViewLayout = layout
         
+        fetch()
+        
     }
     
+    var list: [String] = []
+    
+    var startPage = 1
+    var totalCount = 0
+    
+    func fetch() {
+//        let url = EndPoint.tmdbURL + "/movie/week?api_key=" + APIKey.TMDB_SECRET
+        let url = "https://api.themoviedb.org/3/trending/movie/week?api_key=\(APIKey.TMDB_SECRET)"
+        AF.request(url, method: .get).validate().responseData { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print( "JSON: \(json)")
+                
+                self.totalCount = json["total_results"].intValue
+                
+                for item in json["results"].arrayValue {
+                    self.list.append(item["poster_path"].stringValue)
+                }
+                
+                self.collectionView.reloadData()
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
     
 }
 
+// MARK: Pagenation
+extension SearchViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            if list.count - 1 == indexPath.item && list.count < totalCount {
+                startPage += 30
+                fetch()
+            }
+        }
+        print("===\(indexPaths)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        print("===취소 \(indexPaths)")
+    }
+}
 
 // MARK: CollectionView
 extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return list.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionViewCell", for: indexPath) as? SearchCollectionViewCell else { return UICollectionViewCell() }
         
+        let url = URL(string: EndPoint.tmdbURL + list[indexPath.item])
+        cell.imageView.kf.setImage(with: url)
+        
         cell.backgroundColor = .lightGray
+        
+        print("list", list)
         
         return cell
     }
